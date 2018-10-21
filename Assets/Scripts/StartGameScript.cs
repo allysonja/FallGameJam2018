2a45b17c0117b85ecc337ac7b2041ba59dc1ef8f
allysonja <sonjamw17@gmail.com>
2018-10-21 10:18:59 -0400

    Basic start game script set up

---
+++ Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameScript : MonoBehaviour {
+
+	public void changeScene() {
+		SceneManager.LoadScene("combination_of_ryan_and_player-pickup");
+	}
+	
+}
